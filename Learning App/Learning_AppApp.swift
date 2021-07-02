393632c86db0bd29f224bb7c3934fcfb06da21aa
Joon Park <aisgandy@hotmail.com>
2021-07-03 02:21:03 +0900

    Initial Commit

---
+++ Learning App/Learning_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_AppApp.swift
+//  Learning App
+//
+//  Created by 박준우 on 2021/07/03.
+//
+
+import SwiftUI
+
+@main
+struct Learning_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
